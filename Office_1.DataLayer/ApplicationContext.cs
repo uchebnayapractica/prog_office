5c94e7968afd6e1a27e92944d14a27c304fa1021
NolikTop <aydansuper12@gmail.com>
2022-05-31 20:05:47 +0300

    заливаю

--- Office_1.DataLayer/ApplicationContext.cs
+++ Office_1.DataLayer/ApplicationContext.cs
@@ -1,10 +1,30 @@
 using Microsoft.EntityFrameworkCore;
+using Office_1.DataLayer.Models;
 
 namespace Office_1.DataLayer;
 
-public class ApplicationContext : DbContext
+public sealed class ApplicationContext : DbContext
 {
     
-    
-    
+    public DbSet<Request> Requests { get; set; }
+    public DbSet<Client> Clients { get; set; }
+
+    public ApplicationContext()
+    {
+        Database.EnsureCreated();
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite("FileName=Application.db");
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        /*modelBuilder.Entity<Request>()
+            .HasOne(request => request.Client)
+            .WithMany(client => client.Requests)
+            .HasForeignKey(request => request.IdClient);*/
+    }
+
 }
---
+++ Office_1.DataLayer/Services/ClientService.cs
@@ -0,0 +1,52 @@
+using Office_1.DataLayer.Models;
+
+namespace Office_1.DataLayer.Services;
+
+public static class ClientService
+{
+
+    public static IEnumerable<Client> GetClientsByPrefixOfName(string prefixOfName)
+    {
+        using var context = new ApplicationContext();
+
+        return context.Clients.Where(c => c.Name.StartsWith(prefixOfName));
+    }
+
+    public static Client GetOrCreateClientByName(string name, string address)
+    {
+        using var context = new ApplicationContext();
+
+        var clients = context.Clients.Where(c => c.Name.Equals(name));
+
+        if (clients.Any()) // клиент уже есть в базе
+        {
+            return clients.First();
+        }
+        
+        // клиента еще нет в базе
+        var c = new Client
+        {
+            Name = name,
+            Address = address
+        };
+        
+        InsertClient(c);
+
+        return c;
+    }
+    
+    public static IEnumerable<Client> GetAllClients()
+    {
+        using var context = new ApplicationContext();
+
+        return context.Clients;
+    }
+    
+    public static void InsertClient(Client client)
+    {
+        using var context = new ApplicationContext();
+
+        context.Clients.Add(client);
+    }
+
+}
